075837aee9091b404e6a79aab98a4b648a000f87
Victor Salome <vsalome41@gmail.com>
2024-06-21 13:18:32 -0300

    feat:

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,34 +1,65 @@
-import { useState } from "react";
-import { IValidationLogin } from "../interfaces/interface-login";
-import { validateEmail, validatePassword } from "../utils/validate-login";
-
+import { useState } from 'react';
+import { IValidationLogin } from '../interfaces/interface-login';
+import { validateEmail, validatePassword } from '../utils/validate-login';
+import { ApolloError, useMutation } from '@apollo/client';
+import { LOGIN_MUTATION } from '../graphql/query';
 
 export const LoginPage = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   const [errors, setErrors] = useState<IValidationLogin>({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
+  });
+  const [serverError, setServerError] = useState<string>('');
+  console.log(serverError);
+  const [login] = useMutation(LOGIN_MUTATION, {
+    onCompleted: (data) => {
+      localStorage.setItem('token', data.login.token);
+      console.log('Login Successful', data);
+    },
+    onError: (error: ApolloError) => {
+      if (error.graphQLErrors.length > 0) {
+        setServerError(error.graphQLErrors[0].message);
+      } else {
+        console.log('Error', error);
+      }
+    },
   });
 
-  
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setErrors((prevErrors) => ({ ...prevErrors, email: '' }));
+    setServerError('');
+  };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    setErrors((prevErrors) => ({ ...prevErrors, password: '' }));
+    setServerError('');
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const emailError = validateEmail(email);
     const passwordError = validatePassword(password);
 
     setErrors({ email: emailError, password: passwordError });
+    setServerError('');
 
     if (!emailError && !passwordError) {
-      console.log("Formulário válido. Dados podem ser enviados para o servidor.");
+      try {
+        await login({ variables: { email, password } });
+      } catch (error: any) {
+        if (error.graphQLErrors.length > 0) {
+          setServerError(error.graphQLErrors[0].message);
+        } else {
+          console.error('Error:', error);
+        }
+      }
     }
-   
   };
-
-
-
 
   return (
     <main>
@@ -38,23 +69,15 @@ export const LoginPage = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-            {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
+          <input value={email} onChange={handleEmailChange} />
+          {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
         </div>
         <div>
           <label>Senha:</label>
-          <input
-          type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-            {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
+          <input type='password' value={password} onChange={handlePasswordChange} />
+          {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
         </div>
-
+        {serverError && <p style={{ color: 'red' }}>{serverError}</p>}
         <button type='submit'>Entrar</button>
       </form>
     </main>
